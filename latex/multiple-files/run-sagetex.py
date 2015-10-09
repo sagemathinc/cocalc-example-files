65b894dee536c7d31a41f58dd5c3ea8fa4b04142
Harald Schilly <harald.schilly@gmail.com>
2015-10-09 13:33:22 +0000

    latex multifile: small fixes

--- latex/multiple-files/run-sagetex.py
+++ latex/multiple-files/run-sagetex.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+# This is a small wrapper script, because running sage on a
+# *.sagetex.sage file in a subdirectory is broken. Once this
+# works fine, and produces the sout file in the subdirectory,
+# this script is no longer necessary -> modify latexmkrc !
 import sys, os
 print("run-sagetex.py -- START".center(80, "-"))
-dir, file = sys.argv[1].split("/")
+dir, file = sys.argv[1].split(os.path.sep)
 os.system("cd %s && sage %s" % (dir, file))
 print("run-sagetex.py -- END".center(80, "-"))
